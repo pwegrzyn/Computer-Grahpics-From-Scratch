58a2666784babd17fdad4f83313ec900f430ec3e
Patryk Węgrzyn <wegpat@gmail.com>
2019-05-09 20:04:23 +0200

    Fix Painter

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace render;
 
 int main(int argc, char** argv) {
     
-    render::Painter painter(800, 600, "result.ppm");
+    Painter painter(800, 600, "result.ppm");
     painter.init_img();
     painter.cleanup();
 
